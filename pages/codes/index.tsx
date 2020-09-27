3fe896cc06ed68e6a5f54d4b1d8c82f35238a0f1
gonzalo <zalo250@gmail.com>
2020-09-27 20:34:29 +0200

    WIP: codes.

---
+++ pages/codes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Codes() {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">Here's some things I've done 🙈</div>
+        </div>
+      </div>
+    </div>
+  )
+}
