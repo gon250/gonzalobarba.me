b2714d7e28ca52105478f60ab81991e269a3190d
Gonzalo <zalo250@gmail.com>
2022-01-04 16:42:41 +0100

    update primary color

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,7 +5,5 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return (
-    <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0 font-mono">{children}</div>
-  )
+  return <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
 }
