c1161381442bc6b80659bf79543d7cd0f5491463
Gonzalo Barba <gonzalo@Gonzalos-MacBook-Pro.local>
2024-04-25 15:51:27 +0200

    Add shareplaylist and supahabits

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import profilePic from "../public/me.jpg";
 
 import PlaningPokerLogo from "../public/poker-logo.svg";
+import SupaHabitsLogo from "../public/supahabits-logo.png";
 
 export default function Home() {
   return (
@@ -91,6 +92,54 @@ export default function Home() {
           <section className="lg:h-screen lg:overflow-scroll noscrollbar w-full max-w-[1100px]">
             <div className="divider px-6 my-0 lg:hidden "></div>
             <ul className="max-lg:space-y-4 p-6 lg:p-16 lg:grid lg:grid-cols-2 lg:gap-8">
+              <li className="card duration-200 h-min col-span-1">
+                <a
+                  className="cursor-pointer block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                  href="https://www.supahabits.com"
+                  target="_blank" rel="noreferrer"
+                >
+                  <div className="flex items-center gap-y-1 gap-x-2 lg:gap-x-3 flex-wrap">
+                    <Image alt="PermanentLink Logo"
+                           width={20}
+                           height={20}
+                           className="w-5 h-5 lg:w-6 lg:h-6 w-5 h-5 lg:w-6 lg:h-6"
+                           src={SupaHabitsLogo} />
+                    <p className="mr-auto font-bold lg:text-lg">SupaHabits</p>
+                    <div className="flex gap-2">
+                      <span
+                        className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded border border-green-400">
+                        Active
+                      </span>
+                    </div>
+                  </div>
+                  <p className="text-base-content/80 text-sm lg:text-base">
+                    Creating beneficial habits and finding solutions to common problems.
+                  </p>
+                </a>
+              </li>
+              <li className="card duration-200 h-min col-span-1">
+                <a
+                  className="cursor-pointer block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                  href="https://www.shareplaylist.club/"
+                  target="_blank" rel="noreferrer"
+                >
+                  <div className="flex items-center gap-y-1 gap-x-2 lg:gap-x-3 flex-wrap">
+                    <span>
+                      🎸
+                    </span>
+                    <p className="mr-auto font-bold lg:text-lg">SharePlaylist</p>
+                    <div className="flex gap-2">
+                      <span
+                        className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded border border-green-400">
+                        Active
+                      </span>
+                    </div>
+                  </div>
+                  <p className="text-base-content/80 text-sm lg:text-base">
+                    Create music playlists and invite friends to add their favorite tracks.
+                  </p>
+                </a>
+              </li>
               <li className="card duration-200 h-min col-span-1">
                 <a
                   className="cursor-pointer block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
