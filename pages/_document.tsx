06db3a2e60075a9da5cb299b2b2562960e29739c
Gonzalo Barba <zalo250@gmail.com>
2025-04-10 16:49:01 +0200

    Add tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://umami.mardeleva.synology.me/script.js"
+          data-website-id="2f85967f-f2b6-4762-922a-2e7aacaa7daa"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
